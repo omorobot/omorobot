a22c85ac4b332cfb7aaeff7de27644b936900ab5
t-shaped-person <t.shaped.person@gmail.com>
2026-09-19 17:41:48 +0900

    20260919

--- omorobot_teleop/setup.py
+++ omorobot_teleop/setup.py
@@ -1,5 +1,3 @@
-import os # add
-from glob import glob # add
 from setuptools import find_packages, setup
 
 package_name = 'omorobot_teleop'
@@ -12,7 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')), # add
     ],
     install_requires=['setuptools'],
     zip_safe=True,
